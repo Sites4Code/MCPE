723c903e4861d0b80303bfa2248008139de73b4f
Ad5001 <ad5001yt@gmail.com>
2016-12-19 22:07:04 +0100

    WHY? WHY DOESN'T IT WORK???

--- assets/js/pages.js
+++ assets/js/pages.js
@@ -2,12 +2,13 @@
 Page downloader & switcher to make code cleaner.
 By (c) Ad5001 2016
 */
+i = 0;
 articles = document.getElementsByTagName("article");
 console.log(articles);
 /*l = location.pathname.split("/");
 l[l.length - 1] = undefined;
 dir = l.join("/");*/
-for (i = 0; i < articles.length; i = i) {
+while (i < articles.length) {
     console.log("pages/" + articles[i].id + ".html");
     $.get("pages/" + articles[i].id + ".html", function(responseText) {
         setPage(responseText);
