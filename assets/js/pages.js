cd2675ff7808ea2301431b53f70ad2bb9a903771
Ad5001 <ad5001yt@gmail.com>
2016-12-19 23:02:13 +0100

    YES FINALLY !!!! WORKING !!!

--- assets/js/pages.js
+++ assets/js/pages.js
@@ -2,13 +2,13 @@
 Page downloader & switcher to make code cleaner.
 By (c) Ad5001 2016
 */
-i = 0;
+
 articles = document.getElementsByTagName("article");
 console.log(articles);
 /*l = location.pathname.split("/");
 l[l.length - 1] = undefined;
 dir = l.join("/");*/
-while (i < articles.length) {
+for (i = 0; i < articles.length; i++) {
     console.log("pages/" + articles[i].id + ".html");
     $.get("pages/" + articles[i].id + ".html", function(responseText) {
         setPage(responseText);
@@ -17,5 +17,6 @@ while (i < articles.length) {
 
 
 function setPage(text) {
-    document.getElementsByTagName("article")[i++].innerHTML = text + '<div class="close" onclick="location.hash=\'\';">Close</div>';
+    page = text.substr(5).split(" -->")[0]; // Temporary working solution
+    document.getElementById(page).innerHTML = text + '<div class="close" onclick="location.hash=\'\';">Close</div>';
 }
