b9d48c509238db97179b92b98f5df68bae37d44a
Ad5001 <ad5001yt@gmail.com>
2016-12-20 10:47:58 +0100

    Nice progress bar :)

---
+++ assets/js/courseCalc.js
@@ -0,0 +1,13 @@
+/*
+Course calculator - caculate te current part of the course done
+By (c) Ad5001 2016
+*/
+count = document.getElementsByTagName("article").length - 1 // The hidden page ;)
+list = [];
+for (i = 0; i < count; i++) {
+    list[document.getElementsByTagName("article")[i].id] = Math.round(i / count * 100);
+}
+setInterval(function() {
+    document.getElementById("course").value = list[location.hash.substr(1)];
+    document.getElementById("progress").innerHTML = list[location.hash.substr(1)] + "%";
+}, 100);
