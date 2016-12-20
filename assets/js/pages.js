5737cffaa8a29801b6aa05be3a13a21b358c36b3
Ad5001 <ad5001yt@gmail.com>
2016-12-20 12:00:33 +0100

    Time for the quiz !

--- assets/js/pages.js
+++ assets/js/pages.js
@@ -19,4 +19,82 @@ for (i = 0; i < articles.length; i++) {
 function setPage(text) {
     page = text.substr(5).split(" -->")[0]; // Temporary working solution
     document.getElementById(page).innerHTML = text + '<div class="close" onclick="location.hash=\'\';">Close</div>';
+    if (page == "quiz") {
+        /*
+Quiz maker
+By (c) Ad5001 2016
+*/
+
+        QandA = [{
+                question: "Where should you put your main file?",
+                a: "In the src folder",
+                b: "In te src/<Your_Username> folder",
+                c: "In your resources folder",
+                valid: "2"
+            },
+            {
+                question: "How to log a message on the console?",
+                a: "$this->info('My message');",
+                b: "$this->getLogger()->info(My message);",
+                c: "$this->getLogger()->info('My message');",
+                valid: "3"
+            },
+            {
+                question: "What is the class for the sender of a command?",
+                a: "\\pocketmine\\command\\CommandSender",
+                b: "\\pocketmine\\command\\CmdSender",
+                c: "\\pocketmine\\Player",
+                valid: "1"
+            },
+            {
+                question: "How to save the plugin config?",
+                a: "$this->getConfig()->reload();",
+                b: "$this->saveConifg();",
+                c: "$this->getConfig()->save();",
+                valid: "3"
+            },
+            {
+                question: "How much is a tick?",
+                a: "1 tick = 20 seconds",
+                b: "1 tick = 1/20 seconds",
+                c: "1 tick = 20 seconds",
+                valid: "2"
+            },
+        ]
+
+        quizStep = -1;
+        score = 0;
+        question = document.getElementById("question");
+        answers = [document.getElementById("answer1"), document.getElementById("answer2"), document.getElementById("answer3")];
+        nextQuestion();
+
+
+    }
+}
+
+function quizValidate(num) {
+    if (typeof QandA[quizStep + 1] == "undefined") {
+        alert("Your score is : " + score + " / " + quizStep);
+        quizStep = 0;
+        location.hash = "#";
+        return;
+    }
+    if (num == QandA[quizStep].valid) {
+        score++;
+        answers[num - 1].style.backgroundColor = "lime";
+    } else {
+        answers[num - 1].style.backgroundColor = "red";
+    }
+    setTimeout(function() {
+        nextQuestion();
+        answers[num - 1].style.backgroundColor = "gray";
+    }, 2000);
+}
+
+function nextQuestion() {
+    quizStep++;
+    question.innerHTML = QandA[quizStep].question;
+    answers[0].innerHTML = QandA[quizStep].a;
+    answers[1].innerHTML = QandA[quizStep].b;
+    answers[2].innerHTML = QandA[quizStep].c;
 }
